6d0217d76d2b924d283e2d1c759e9b6f765652d4
Bradley Pregon <bradley.pregon@gmail.com>
2022-03-11 14:48:42 -0600

    added refresh, changed some styling

--- NativeWeather/Screens/WeatherScreen.js
+++ NativeWeather/Screens/WeatherScreen.js
@@ -1,58 +1,99 @@
 import React, {useEffect, useState} from 'react';
-import { StyleSheet, Text, View, ScrollView, StatusBar, Button } from 'react-native';
+import { RefreshControl, StyleSheet, Text, View, ScrollView, StatusBar, Button, SafeAreaView } from 'react-native';
 import {OWM_KEY} from '@env';
+
+import data from '../TestAPIData.json';
 
 export default function WeatherScreen() {
   let lat = 41.0076;
   let long = -92.9637
   let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${OWM_KEY}`
-  const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
-  console.log(data);
+  
+  // temporarily setting useState to FALSE and commenting out [data, setData]
+  const [isLoading, setLoading] = useState(false);
+  //const [data, setData] = useState([]);
+  const [refreshing, setRefreshing] = React.useState(false);
 
-  useEffect(() => {
-    fetch(url)
-    .then((response) => response.json())
-    .then((json) => setData(json))
-    .catch((error) => console.error(error))
-    .finally(() => setLoading(false))
+
+  // Temporarily Disabling for testing - no need to call API every time the app is saved!
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    fetchWeather()
   }, []);
 
+  useEffect(() => {
+    fetchWeather();
+  }, []);
+
+  const fetchWeather = () => {
+
+    /*
+    fetch(url)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setRefreshing(false);
+        setLoading(false);
+        setData(responseJson);
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+      */
+  }
+
   return (
-    <ScrollView style={styles.scrollView}>
+    <SafeAreaView style={styles.container}>
+    <ScrollView 
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }>
+      
       {
       isLoading ? <Text>Loading...</Text> : 
-        <View style={styles.container}>
+        <View style={styles.viewContainer}>
           <Text style={styles.cityText}>Packwood</Text>
-          <Text style={styles.temperatureText}>{data.current.temp}</Text>
-          <Text>{data.current.weather[0].description}</Text>
-          <Text>H: {data.daily[0].temp.max} L: {data.daily[0].temp.min}</Text>
+          <Text style={styles.temperatureText}>{Math.floor(data.current.temp)}°</Text>
+          
+          <Text style={styles.detailText}>{data.current.weather[0].description}</Text>
+          
+          <Text style={styles.detailText}>H: {Math.floor(data.daily[0].temp.max)}° L: {Math.floor(data.daily[0].temp.min)}°</Text>
         </View>
       }
     </ScrollView>
+    </SafeAreaView>
+    
   );
 }
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#AAA',
       alignItems: 'center',
       justifyContent: 'center',
-    },
-    scrollView: {
       backgroundColor: '#AAA',
-      paddingTop: StatusBar.currentHeight + 30,
+    },
+    viewContainer: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingTop: StatusBar.currentHeight + 20,
     },
     cityText: {
-      fontFamily: 'sans-serif-light',
-      fontSize: 30,
-      shadowColor: '#AAA',
-      shadowRadius: 2
+      fontSize: 40,
+      color: 'white',
+      textShadowColor: '#666',
+      textShadowRadius: 3,
+      textShadowOffset: {width: 0, height: 2}
     },
     temperatureText: {
-      fontSize: 60,
-      shadowColor: '#AAA',
-      shadowRadius: 2
+      fontSize: 80,
+      color: 'white',
+      textShadowColor: '#666',
+      textShadowRadius: 3,
+      textShadowOffset: {width: 0, height: 2}
+    },
+    detailText: {
+      color: 'white',
+      fontSize: 17,
+      fontWeight: '600'
     },
   });
