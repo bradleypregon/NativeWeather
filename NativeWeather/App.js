ff2c7f9efe3b92e8bc5e75d92ac1024acd97aba8
Bradley Pregon <bradley.pregon@gmail.com>
2022-03-12 10:14:35 -0600

    added settings screen

--- NativeWeather/App.js
+++ NativeWeather/App.js
@@ -1,9 +1,12 @@
-import { StyleSheet } from 'react-native';
-import { NavigationContainer, TabActions } from '@react-navigation/native';
+import {  StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import WeatherScreen from './Screens/WeatherScreen';
 import RadarScreen from './Screens/RadarScreen';
+import SettingsScreen from './Screens/SettingsScreen';
+
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,8 +14,27 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator screenOptions={{headerShown: false}}>
-        <Tab.Screen name="Weather" component={WeatherScreen} />
-        <Tab.Screen name="Radar" component={RadarScreen} />
+        <Tab.Screen 
+          name="Weather"
+          options={{
+            tabBarIcon: ({color}) => <Ionicons name="ios-partly-sunny-sharp" size={25} color={color}/>
+          }} 
+          component={WeatherScreen}
+        />
+        <Tab.Screen 
+          name="Radar" 
+          options={{
+            tabBarIcon: ({color}) => <Ionicons name="map" size={25} color={color}/>
+          }}
+          component={RadarScreen} 
+        />
+        <Tab.Screen 
+          name="Settings" 
+          options={{
+            tabBarIcon: ({color}) => <Ionicons name="settings" size={25} color={color}/>
+          }}
+          component={SettingsScreen} 
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
